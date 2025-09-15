8d123c218010e628187be680c2a074c05face02d
daun <wjdekdns0218@gmail.com>
2025-09-15 13:39:56 +0900

    프로그래머스 알고리즘 문제 풀이 - 배열 181856 수정

--- daun/Solution_181856.java
+++ daun/Solution_181856.java
@@ -2,12 +2,10 @@ package daun;
 
 class Solution_181856 {
     public int solution(int[] arr1, int[] arr2) {
-        int answer = 0;
-
         if (arr1.length > arr2.length){
-            answer = 1;
+            return 1;
         } else if (arr1.length < arr2.length){
-            answer = -1;
+            return -1;
         }
 
         int diff = 0;
