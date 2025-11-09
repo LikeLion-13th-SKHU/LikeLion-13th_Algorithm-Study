195fed3c5bc52fcaacb238a131af975511272a07
Kim, Sujeong <qpaalaai@gmail.com>
2025-11-09 23:47:44 +0900

    [BOJ]10816 숫자 카드2/실버4/40분

---
+++ sujeong/BOJ10816/Main.java
@@ -0,0 +1,41 @@
+// 숫자 카드 2
+
+package BOJ10816;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashMap;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        int N = Integer.parseInt(br.readLine());
+
+        HashMap<Integer, Integer> map = new HashMap<>();
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        for (int i = 0; i < N; i++) {
+            int card = Integer.parseInt(st.nextToken());
+
+            map.put(card, map.getOrDefault(card, 0) + 1);
+        }
+
+        int M = Integer.parseInt(br.readLine());
+
+        st = new StringTokenizer(br.readLine(), " ");
+        for (int i = 0; i < M; i++) {
+            int num = Integer.parseInt(st.nextToken());
+
+            int count = map.getOrDefault(num, 0);
+
+            sb.append(count).append(" ");
+        }
+
+        System.out.println(sb.toString().trim());
+    }
+}
