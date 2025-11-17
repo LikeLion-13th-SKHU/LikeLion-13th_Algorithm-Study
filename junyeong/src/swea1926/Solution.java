71434a7a81921703e36905675abf017ae51b51c2
Junyeong-An <dev.jun0321@gmail.com>
2025-11-17 23:02:23 +0900

    [SWEA] 어디에 단어가 들어갈 수 있을까 / D2 / 15분

---
+++ junyeong/src/swea1926/Solution.java
@@ -0,0 +1,18 @@
+package swea1926;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+
+
+        for(int i=1;i<=n;i++){
+
+        }
+    }
+}
