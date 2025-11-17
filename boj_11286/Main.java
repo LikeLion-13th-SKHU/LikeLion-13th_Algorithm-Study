17d57629db1b8ef46355e4e04bd2102879b237bd
daun <wjdekdns0218@gmail.com>
2025-11-17 22:14:39 +0900

    백준 알고리즘 문제 풀이 - 우선순위큐 11286

---
+++ boj_11286/Main.java
@@ -0,0 +1,33 @@
+package boj_11286;
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        int N = Integer.parseInt(br.readLine());
+
+        PriorityQueue<Integer> pq = new PriorityQueue<>((a, b) -> {
+            int absA = Math.abs(a);
+            int absB = Math.abs(b);
+            if (absA == absB) return a - b;
+            return absA - absB;
+        });
+
+        for (int i = 0; i < N; i++) {
+            int x = Integer.parseInt(br.readLine());
+
+            if (x == 0) {
+                if (pq.isEmpty()) sb.append(0).append("\n");
+                else sb.append(pq.poll()).append("\n");
+            } else {
+                pq.add(x);
+            }
+        }
+
+        System.out.print(sb);
+    }
+}
